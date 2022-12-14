0e0abc3eca610cb304d716a9be520f912a869a7d
Andy Farquharson <andyfarquharson@gmail.com>
2022-12-14 14:51:36 +0000

    Add a lyrical about section

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  get 'about/index'
   root to: 'products#index'
 
   resources :products, only: [:index, :show]
@@ -17,6 +18,8 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
   end
 
+  get '/about', to: 'about#index'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
