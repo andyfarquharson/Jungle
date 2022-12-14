f2132a9321d4b2dc3b79e7716750927db26b4c5f
Andy Farquharson <andyfarquharson@gmail.com>
2022-12-14 22:10:56 +0000

    Fix bug in new form categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
 
-  namespace :admin do
-    get 'categories/index'
-    get 'categories/new'
-    get 'categories/create'
-  end
   get 'about/index'
   root to: 'products#index'
 
@@ -21,6 +16,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, except: [:edit, :update, :show]
   end
 
   get '/about', to: 'about#index'
