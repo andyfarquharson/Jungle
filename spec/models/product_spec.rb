c13271cc70b35135761730e29781c651f17a06c4
Andy Farquharson <andyfarquharson@gmail.com>
2022-12-17 17:49:48 +0000

    Verify that products save when all information is present

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,57 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+
+    it "should save a product" do
+      @category = Category.create(name:"Ducks")
+      product = Product.new(
+        name: "Muscovy",
+        price: 21.99,
+        quantity: 3,
+        category_id: @category.id
+      )
+    expect(product).to(be_valid)
+    end
+    it "should not save without a valid name" do
+      @category = Category.create(name:"Ducks")
+      product = Product.new(
+        name: nil,
+        price: 21.99,
+        quantity: 3,
+        category_id: @category.id
+      )
+    expect(product).to_not(be_valid)
+    end
+    it "should not save without a valid price" do
+      @category = Category.create(name:"Ducks")
+      product = Product.new(
+        name: "Muscovy",
+        price: nil,
+        quantity: 3,
+        category_id: @category.id
+      )
+    expect(product).to(be_valid)
+    end
+    it "should not save without a valid quantity" do
+      @category = Category.create(name:"Ducks")
+      product = Product.new(
+        name: "Muscovy",
+        price: 21.99,
+        quantity: nil,
+        category_id: @category.id
+      )
+    expect(product).to_not(be_valid)
+    end
+    it "should not save without a valid category" do
+      @category = Category.create(name:"Ducks")
+      product = Product.new(
+        name: "Muscovy",
+        price: 21.99,
+        quantity: 3,
+        category_id: nil
+      )
+    expect(product).to_not(be_valid)
+    end
+  end
+end
